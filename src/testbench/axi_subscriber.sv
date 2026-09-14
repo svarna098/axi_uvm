9210ae3e1a0ede110308d85e026dfcb974d2965e
svarna098 <svarna098@gmail.com>
2026-09-14 23:37:59 +0530

    Update axi_subscriber.sv

--- src/testbench/axi_subscriber.sv
+++ src/testbench/axi_subscriber.sv
@@ -30,16 +30,16 @@ function void report_phase(uvm_phase phase);
 super.report_phase(phase);
     `uvm_info(get_name,$sformatf(" COVERAGE = %0f",cg.get_coverage()),UVM_NONE)
      
-  `uvm_info("COVERAGE",$sformatf("awaddr  = %0.2f%%",cg.a.get_coverage()),UVM_LOW)
-     `uvm_info("COVERAGE",$sformatf("awprot  = %0.2f%%",cg.b.get_coverage()),UVM_LOW)
-     `uvm_info("COVERAGE",$sformatf("awvalid  = %0.2f%%",cg.c.get_coverage()),UVM_LOW)
-       `uvm_info("COVERAGE",$sformatf("wdata  = %0.2f%%",cg.d.get_coverage()),UVM_LOW)
-        `uvm_info("COVERAGE",$sformatf("wstrb  = %0.2f%%",cg.e.get_coverage()),UVM_LOW)
-        `uvm_info("COVERAGE",$sformatf("wvalid  = %0.2f%%",cg.f.get_coverage()),UVM_LOW)
-        `uvm_info("COVERAGE",$sformatf("bready  = %0.2f%%",cg.g.get_coverage()),UVM_LOW)
-          `uvm_info("COVERAGE",$sformatf(" araddr = %0.2f%%",cg.h.get_coverage()),UVM_LOW)
-         `uvm_info("COVERAGE",$sformatf("arvalid  = %0.2f%%",cg.j.get_coverage()),UVM_LOW)
-     `uvm_info("COVERAGE",$sformatf("rready  = %0.2f%%",cg.k.get_coverage()),UVM_LOW)
+  `uvm_info("COVERAGE",$sformatf("awaddr = %0.2f%%",cg.a.get_coverage()),UVM_LOW)
+     `uvm_info("COVERAGE",$sformatf("awprot = %0.2f%%",cg.b.get_coverage()),UVM_LOW)
+     `uvm_info("COVERAGE",$sformatf("awvalid = %0.2f%%",cg.c.get_coverage()),UVM_LOW)
+       `uvm_info("COVERAGE",$sformatf("wdata = %0.2f%%",cg.d.get_coverage()),UVM_LOW)
+        `uvm_info("COVERAGE",$sformatf("wstrb = %0.2f%%",cg.e.get_coverage()),UVM_LOW)
+        `uvm_info("COVERAGE",$sformatf("wvalid = %0.2f%%",cg.f.get_coverage()),UVM_LOW)
+        `uvm_info("COVERAGE",$sformatf("bready = %0.2f%%",cg.g.get_coverage()),UVM_LOW)
+          `uvm_info("COVERAGE",$sformatf("araddr= %0.2f%%",cg.h.get_coverage()),UVM_LOW)
+         `uvm_info("COVERAGE",$sformatf("arvalid = %0.2f%%",cg.j.get_coverage()),UVM_LOW)
+     `uvm_info("COVERAGE",$sformatf("rready = %0.2f%%",cg.k.get_coverage()),UVM_LOW)
 endfunction
 
  
